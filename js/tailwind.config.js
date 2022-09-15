b71bc4f26b67ce8d306dece0f019492b0d5516a3
Sajid U. Abir <sajidabir01@gmail.com>
2022-09-15 15:39:03 +0600

    initial commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'blue' : '#6A4DF4',
+          'lightBlue' : '#F8F6FE',
+          'pera' : '#737373',
+          'black' : '#151515',
+          'gray': '#F3F3F3',
+        },
+        fontFamily: {
+          'Inter' : ['Inter'],
+          },
+        maxWidth: {
+            'container': '1320px',
+            
+        }
+      }
+    }
+  }
